b2abd3d9911d123835737e2e929e5e91949a2011
Junya1230 <yamadajunyadesu@gmail.com>
2025-07-12 08:51:10 +0900

    selenium自動テストソース格納先移動

--- src/tests/selenium/selenium_test.php
+++ src/tests/selenium/selenium_test.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 
 use Facebook\WebDriver\Remote\RemoteWebDriver;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
